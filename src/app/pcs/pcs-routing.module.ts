a150e754fc9388661ee14fcbac7ba6f903c6bc89
nihkilrstg18 <nikhilrstg18@gmail.com>
2021-03-06 12:12:29 +0530

    added route config in pcs-routing.module improved pcs.module

--- src/app/pcs/pcs-routing.module.ts
+++ src/app/pcs/pcs-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PcsFiltersComponent } from './components/pcs-filters/pcs-filters.component';
+import { PcsGridComponent } from './components/pcs-grid/pcs-grid.component';
+import { PcDetailComponent } from './pages/pc-detail/pc-detail.component';
+import { PcsComponent } from './pages/pcs/pcs.component';
 
-const routes: Routes = [];
+const PCS_ROUTES: Routes = [
+  {
+    path: '',
+    component: PcsComponent,
+  },
+  {
+    path: ':serviceTag',
+    component: PcDetailComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(PCS_ROUTES)],
+  exports: [RouterModule],
 })
-export class PcsRoutingModule { }
+export class PcsRoutingModule {
+  static components: any[] = [
+    PcsComponent,
+    PcsFiltersComponent,
+    PcsGridComponent,
+    PcDetailComponent,
+  ];
+}
--- src/app/pcs/pcs.module.ts
+++ src/app/pcs/pcs.module.ts
@@ -1,18 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { PcsRoutingModule } from './pcs-routing.module';
-import { PcsComponent } from './pages/pcs/pcs.component';
-import { PcDetailComponent } from './pages/pc-detail/pc-detail.component';
-import { PcsFiltersComponent } from './pages/pcs-filters/pcs-filters.component';
-import { PcsGridComponent } from './components/pcs-grid/pcs-grid.component';
-
 
 @NgModule({
-  declarations: [PcsComponent, PcDetailComponent, PcsFiltersComponent, PcsGridComponent],
-  imports: [
-    CommonModule,
-    PcsRoutingModule
-  ]
+  declarations: [...PcsRoutingModule.components],
+  imports: [CommonModule, PcsRoutingModule],
 })
-export class PcsModule { }
+export class PcsModule {}
