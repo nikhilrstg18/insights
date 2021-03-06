03f2d6b5c1d469950f43d76730b36d8a10b19f40
nihkilrstg18 <nikhilrstg18@gmail.com>
2021-03-06 12:05:53 +0530

    added lazy loading in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((d) => d.DashboardModule),
+  },
+  {
+    path: 'pcs',
+    loadChildren: () => import('./pcs/pcs.module').then((p) => p.PcsModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
