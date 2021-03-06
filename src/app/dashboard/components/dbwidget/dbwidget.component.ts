20af33aa4ab6c82a0d244501e53ffef232909e0c
nihkilrstg18 <nikhilrstg18@gmail.com>
2021-03-06 17:10:11 +0530

    Created dbWidget component

--- src/app/dashboard/components/dbwidget/dbwidget.component.ts
+++ src/app/dashboard/components/dbwidget/dbwidget.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { DBWidget } from './../../models/db-widget';
+import { Component, Input, OnInit } from '@angular/core';
+// import '@cds/core/icon/register.js';
+// import { ClarityIcons, bellIcon } from '@cds/core/icon';
 
 @Component({
   selector: 'i-dbwidget',
   templateUrl: './dbwidget.component.html',
-  styleUrls: ['./dbwidget.component.scss']
+  styleUrls: ['./dbwidget.component.scss'],
 })
 export class DBWidgetComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() public issue: DBWidget = new DBWidget();
+  constructor() {
+    // ClarityIcons.addIcons(bellIcon);
   }
 
+  ngOnInit(): void {}
 }
---
+++ src/app/dashboard/models/db-widget.ts
@@ -0,0 +1,10 @@
+export class DBWidget {
+  constructor(
+    public title: string = '',
+    public iconShape: string = '',
+    public captionTemplate: string = '',
+    public threshold: number = 0,
+    public count: number = 0,
+    public csat: number = 0
+  ) {}
+}
