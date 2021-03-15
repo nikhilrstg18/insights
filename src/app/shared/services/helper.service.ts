2d3de998792da6a4db0022c6d01c4afb6b58c16d
nihkilrstg18 <nikhilrstg18@gmail.com>
2021-03-15 17:04:11 +0530

    abstracted getDefaultCardDataList in helperService

--- src/app/shared/services/helper.service.ts
+++ src/app/shared/services/helper.service.ts
@@ -7,11 +7,170 @@ import { CpuEnum } from '../enums/cpu.enum'
 import { MetricNameEnum } from '../enums/metric-name.enum'
 import { Endpoint } from '../models/endpoint'
 import { PcdUsageCard } from '../models/pcd-usage-card'
+import { Params } from '@angular/router'
+import { FilterCard } from '../models/filter-card'
 
 @Injectable({
 	providedIn: 'root',
 })
 export class HelperService {
+	getDefaultCardDataList(queryParams: Params) {
+		return [
+			new FilterCard(
+				MetricEnum.OS_FAILURES,
+				MetricNameEnum.OS_FAILURES,
+				'>=',
+				Object.keys(queryParams)[0] == MetricEnum.OS_FAILURES,
+				Object.keys(queryParams)[0] == MetricEnum.OS_FAILURES
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: 10,
+				10,
+				0,
+				20,
+				'PCs with',
+				'OS failures',
+				'',
+				['0', '10', '20']
+			),
+			new FilterCard(
+				MetricEnum.APP_FAILURES,
+				MetricNameEnum.APP_FAILURES,
+				'>=',
+				Object.keys(queryParams)[0] == MetricEnum.APP_FAILURES,
+				Object.keys(queryParams)[0] == MetricEnum.APP_FAILURES
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: 15,
+				15,
+				0,
+				30,
+				'PCs with',
+				'App failures',
+				'',
+				['0', '15', '30']
+			),
+			new FilterCard(
+				MetricEnum.AGE,
+				MetricNameEnum.AGE,
+				'>=',
+				Object.keys(queryParams)[0] == MetricEnum.AGE,
+				Object.keys(queryParams)[0] == MetricEnum.AGE
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: 24,
+				24,
+				0,
+				48,
+				'PCs shipped',
+				'months ago',
+				'',
+				['0mos.', '24mos.', '48mos.']
+			),
+			new FilterCard(
+				MetricEnum.CPU_UTIL,
+				MetricNameEnum.CPU_UTIL,
+				'==',
+				Object.keys(queryParams)[0] == MetricEnum.CPU_UTIL,
+				Object.keys(queryParams)[0] == MetricEnum.CPU_UTIL
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: CpuEnum.MEDIUM,
+				CpuEnum.MEDIUM,
+				CpuEnum.NORMAL,
+				CpuEnum.HIGH,
+				'PCs with',
+				'CPU usage',
+				'',
+				['N', 'M', 'H'],
+				1,
+				{
+					[CpuEnum.NA]: 'NA',
+					[CpuEnum.NORMAL]: 'normal',
+					[CpuEnum.MEDIUM]: 'medium',
+					[CpuEnum.HIGH]: 'high',
+				}
+			),
+			new FilterCard(
+				MetricEnum.RAM_UTIL,
+				MetricNameEnum.RAM_UTIL,
+				'>=',
+				Object.keys(queryParams)[0] == MetricEnum.RAM_UTIL,
+				Object.keys(queryParams)[0] == MetricEnum.RAM_UTIL
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: 50,
+				50,
+				0,
+				100,
+				'PCs with',
+				'RAM usage',
+				'%',
+				['0%', '50%', '100%']
+			),
+			new FilterCard(
+				MetricEnum.RAM,
+				MetricNameEnum.RAM,
+				'<=',
+				Object.keys(queryParams)[0] == MetricEnum.RAM,
+				Object.keys(queryParams)[0] == MetricEnum.RAM
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: 32,
+				32,
+				4,
+				64,
+				'PCs with',
+				'installed memory',
+				'GB',
+				['4GB', '32GB', '64GB'],
+				4
+			),
+			new FilterCard(
+				MetricEnum.STORAGE_REMAINING,
+				MetricNameEnum.STORAGE_REMAINING,
+				'<=',
+				Object.keys(queryParams)[0] == MetricEnum.STORAGE_REMAINING,
+				Object.keys(queryParams)[0] == MetricEnum.STORAGE_REMAINING
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: 50,
+				50,
+				0,
+				100,
+				'PCs with',
+				'storage space available',
+				'%',
+				['0%', '50%', '100%']
+			),
+			new FilterCard(
+				MetricEnum.BATTERY_HEALTH,
+				MetricNameEnum.BATTERY_HEALTH,
+				'<=',
+				Object.keys(queryParams)[0] == MetricEnum.BATTERY_HEALTH,
+				Object.keys(queryParams)[0] == MetricEnum.BATTERY_HEALTH
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: 50,
+				50,
+				0,
+				100,
+				'PCs with',
+				'overall battery performance',
+				'%',
+				['0%', '50%', '100%']
+			),
+			new FilterCard(
+				MetricEnum.BATTERY_RUNTIME,
+				MetricNameEnum.BATTERY_RUNTIME,
+				'<=',
+				Object.keys(queryParams)[0] == MetricEnum.BATTERY_RUNTIME,
+				Object.keys(queryParams)[0] == MetricEnum.BATTERY_RUNTIME
+					? Number(queryParams[Object.keys(queryParams)[0]])
+					: 6,
+				6,
+				0,
+				12,
+				'PCs with',
+				'battery life',
+				'hrs',
+				['0hrs', '6hrs', '12hrs']
+			),
+		]
+	}
+
 	public getSeverityColor(
 		title: string,
 		metric: number,
